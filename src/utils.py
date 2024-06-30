d6ae1fcf7b74345fab07d8177baa9529c62a51b9
Aditya-NeuralNetNinja <adityachrs7@gmail.com>
2024-06-30 22:44:25 +0530

    Utils file added

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,15 @@ import os
 import pandas as pd
 import mysql.connector as mysql
 from dotenv import load_dotenv
-from typing import Optional, Tuple, DataFrame
+from typing import Optional, Tuple
+from pandas import DataFrame
 
 load_dotenv()
 
 # STEP 1 - connect to server
 def connector(user:str,
               host:str,
-              db:Optional[str]=None) -> Tuple[str,str]:
+              db:Optional[str]=None) -> Tuple[mysql.MySQLConnection, mysql.cursor.MySQLCursor]:
     """
     Connect via python to mysql server
 
@@ -30,15 +31,18 @@ def connector(user:str,
     return cnx, cur
 
 # STEP 2 - Create db
-def build_db(cur:str, db:str) -> None:
+def build_db(cur:mysql.cursor.MySQLCursor, db:str) -> None:
     """
     Build db
 
     Args:
         db (str): Input db name
     """
-    {cur}.execute(f'DROP DATABASE IF EXISTS {db};')
-    {cur}.execute(f'CREATE DATABASE {db};')
+    cur.execute(f'DROP DATABASE IF EXISTS {db};')
+    cur.execute(f'CREATE DATABASE {db};')
+    cur.execute('SHOW DATABASES')
+    databases = cur.fetchall()
+    return databases
     
 # STEP 3 - Read CSV data
 def get_data(file_path:str) -> DataFrame:
@@ -52,28 +56,77 @@ def get_data(file_path:str) -> DataFrame:
         DataFrame: resultant dataframe
     """
     df = pd.read_csv(file_path)
+    df.drop(['Unnamed:0'],axis=1,inplace=True,errors='ignore')
     return df
 
-# STEP 4 - Build Schema/Table --- how to connect table to df???
-def build_table(db:str, table:str, col1:str, col2:str) -> None:
+# STEP 4 - Build Schema
+
+def build_schema(df:DataFrame) -> Tuple[str, str]:
+    """
+    Convert python style datatypes into sql format datatypes of each column in dataframe
+    Args:
+        df (pd.DataFrame): Input dataframe
+    Returns:
+        Tuple[str, str]: Returns result, placeholders
+    """
+    column_dtypes = []
+    for col_name in df.columns:
+        print(col_name, df[col_name].dtype)
+        col_name = col_name.replace('.', '_')  # tackle edge case
+        if df[col_name].dtype == 'object':
+            data_type = 'VARCHAR(255)'
+        elif df[col_name].dtype == 'float64':
+            data_type = 'FLOAT'
+        elif df[col_name].dtype == 'int64':
+            data_type = 'INT'
+        else:
+            data_type = 'VARCHAR(255)'  # Default type
+        column_dtypes.append(f"`{col_name}` {data_type}")
+    result = f"({', '.join(column_dtypes)})"
+    placeholders = ', '.join(['%s'] * len(df.columns))
+    
+    return result, placeholders
+
+# STEP 5 - Build Table
+def build_table(cur:mysql.cursor.MySQLCursor, db: str, table: str, schema: str) -> None:
     """
     Create Schema with columns
-
     Args:
+        cur (mysql.cursor.MySQLCursor): MySQL cursor
         db (str): Database name
         table (str): Schema name
-        col1 (str): Name of column 1
-        datatype1 (str): Column 1 datatype
-        col2 (str): Name of column 2
-        datatype2 (str): Column 2 datatype
+        schema (str): Table schema
     """
     cur.execute(f'USE {db};')
     cur.execute(f'DROP TABLE IF EXISTS {table};')
-    cur.execute(f'CREATE TABLE {table}({col1} {df[col1].dtype}, {df[col2].dtype});')
+    cur.execute(f'CREATE TABLE {table} {schema};')
 
-build_table(db='d2p1',
-            table='tutorial',
-            col1='id',
-            datatype1='int',
-            col2='course_name',
-            datatype2='varchar(20)')
+# STEP 6 - Insert Data into Table
+def ingest_data(cur:mysql.cursor.MySQLCursor, cnx:str, df:DataFrame, placeholders:str) -> None:
+    """
+    Insert Data into Table
+
+    Args:
+        cur (str): _description_
+        cnx (str): _description_
+        df (DataFrame): _description_
+        placeholders (str): _description_
+    """
+    total=0
+    for _,row in df.iterrows():
+        sql = f"INSERT INTO customers VALUES({placeholders})"
+        val = tuple(row)
+        cur.execute(sql, val)
+        if cur.rowcount==1:
+            total+=1
+    cnx.commit()
+
+# STEP 7 - Close cursor, connection
+def close_all(cur:mysql.cursor.MySQLCursor, cnx:mysql.MySQLConnection) -> None:
+    """
+    Args:
+        cur (str): cursor
+        cnx (str): connection
+    """
+    cur.close()
+    cnx.close() # type: ignore
