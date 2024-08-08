5fcf26f852dd86227abd94e29a0c1cf1c186044d
Aditya-NeuralNetNinja <adityachrs7@gmail.com>
2024-08-08 22:25:39 +0530

    sql script updated

--- src/join_tables.sql
+++ src/join_tables.sql
@@ -3,5 +3,5 @@ LEFT JOIN sale_processed.sensor_processed
 ON sale_processed.sales_processed.product_id = sale_processed.sensor_processed.product_id;
 
 SELECT * FROM sale_processed.sales_processed
-LEFT JOIN sale_processed.sensor_processed
-ON sale_processed.sales_processed.timestamp = sale_processed.sensor_processed.timestamp;
+LEFT JOIN sale_processed.temp_processed
+ON sale_processed.sales_processed.timestamp = sale_processed.temp_processed.timestamp;
