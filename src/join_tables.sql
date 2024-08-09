acb91bbf3aefac146db8f3ff58875546685efdb0
Aditya-NeuralNetNinja <adityachrs7@gmail.com>
2024-08-09 06:17:24 +0530

    SQL script updated

--- src/join_tables.sql
+++ src/join_tables.sql
@@ -1,7 +1,11 @@
-SELECT * FROM sale_processed.sales_processed
-LEFT JOIN sale_processed.sensor_processed 
-ON sale_processed.sales_processed.product_id = sale_processed.sensor_processed.product_id;
-
-SELECT * FROM sale_processed.sales_processed
-LEFT JOIN sale_processed.temp_processed
-ON sale_processed.sales_processed.timestamp = sale_processed.temp_processed.timestamp;
+SELECT 
+    a.timestamp,
+    a.product_id,
+    a.estimated_stock_pct,
+    b.quantity,
+    c.temperature
+FROM sale_processed.sensor_processed a
+LEFT JOIN sale_processed.sales_processed b
+    ON a.product_id = b.product_id AND a.timestamp = b.timestamp
+LEFT JOIN sale_processed.temp_processed c
+    ON a.timestamp = c.timestamp;
