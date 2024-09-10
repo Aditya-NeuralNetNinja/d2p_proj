38b3c59f36027bfb6b2f51e63c236f053d9f6a52
mitulds <120341649+mitulds@users.noreply.github.com>
2024-09-10 01:28:24 -0600

    Update forecast_extraction.sql

--- src/forecast_extraction.sql
+++ src/forecast_extraction.sql
@@ -1,10 +1,13 @@
 SELECT 
+	a.timestamp,
     a.product_id,
     a.estimated_stock_pct,
-    b.*
+    a.category,
+    a.unit_price,
+    b.quantity,
     c.temperature
-FROM inventory_processed_db.stock_processed a
-LEFT JOIN inventory_processed_db.sales_processed b
+FROM stock_processed a
+LEFT JOIN sales_processed b
     ON a.product_id = b.product_id AND a.timestamp = b.timestamp
-LEFT JOIN inventory_processed_db.temp_processed c
+LEFT JOIN temp_processed c
     ON a.timestamp = c.timestamp;
